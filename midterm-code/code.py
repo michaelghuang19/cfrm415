af790e2c3829ee1a7925d2bbb64b558f5f73099d
Michael Huang <mhuang19@cs.washington.edu>
2021-05-06 15:57:10 -0700

    1a complete

--- midterm-code/code.py
+++ midterm-code/code.py
@@ -18,7 +18,33 @@ def main():
 def a1():
   print("a1")
 
+  mu = c.a1_mu_T.T
+  sigma = c.a1_sigma
+  n, d = sigma.shape
+  ones = np.ones((n, 1))
+
+  omega_num = (linalg.inv(sigma)).dot(ones)
+  omega_den = (ones.T).dot(linalg.inv(sigma))
+  omega_den = omega_den.dot(ones)
+  omega = omega_num / omega_den
+
+  returns = (omega.T).dot(mu)
+
+  # variance < - t(weight_vector) % * % D % * % weight_vector
+
+  variance_denom = (ones.T).dot(linalg.inv(sigma)).dot(ones)
+  variance = 1 / (variance_denom)
+  volatility = np.sqrt(variance)
+
+  # part a responses
+  print(omega)
+  print(returns)
+  print(volatility)
+
+
+
   
 
 if __name__ == "__main__":
   main()
+
--- midterm-code/constants.py
+++ midterm-code/constants.py
@@ -1,8 +1,20 @@
 # Midterm code constants
 
+import numpy as np
+
 run_a1 = True
 run_a2 = True
 run_a3 = True
 run_a4 = True
 run_a5 = True
 run_a6 = True
+
+a1_mu_T = np.matrix([0.3, 0.6, 0.7])
+
+a1_sigma = np.matrix([
+[2.5, 1.2, 0.3],
+[1.2, 3.1, 1.3],
+[0.3, 1.3, 2.4],
+])
+
+
