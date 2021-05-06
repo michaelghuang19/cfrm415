6493bcb6f989dddd86a41473e2c6b50320cdd910
Michael Huang <mhuang19@cs.washington.edu>
2021-05-06 16:23:47 -0700

    draft 1

--- midterm-code/code.py
+++ midterm-code/code.py
@@ -20,11 +20,12 @@ def a1():
 
   mu = c.a1_mu_T.T
   sigma = c.a1_sigma
+  sigma_inv = linalg.inv(sigma)
   n, d = sigma.shape
   ones = np.ones((n, 1))
 
-  omega_num = (linalg.inv(sigma)).dot(ones)
-  omega_den = (ones.T).dot(linalg.inv(sigma))
+  omega_num = (sigma_inv).dot(ones)
+  omega_den = (ones.T).dot(sigma_inv)
   omega_den = omega_den.dot(ones)
   omega = omega_num / omega_den
 
@@ -32,15 +33,18 @@ def a1():
 
   # variance < - t(weight_vector) % * % D % * % weight_vector
 
-  variance_denom = (ones.T).dot(linalg.inv(sigma)).dot(ones)
+  variance_denom = (ones.T).dot(sigma_inv).dot(ones)
   variance = 1 / (variance_denom)
   volatility = np.sqrt(variance)
+
+  A = (ones.T).dot(sigma_inv).dot(ones)
+  B = (mu.T).dot(sigma_inv).dot(ones)
+  C = (mu.T).dot(sigma_inv).dot(mu)
 
   # part a responses
   print(omega)
   print(returns)
   print(volatility)
-
 
 
   
